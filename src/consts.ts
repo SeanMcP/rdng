433206e57204c66052ec2a8e7bfa7dda8309db68
Sean McPherson <sean@seanmcp.com>
2023-06-16 06:47:52 -0400

    initial commit

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Reading.";
+export const SITE_DESCRIPTION =
+  "A website all about reading by an aspiring reading scientist.";
---
+++ src/content/blog/hello-world.md
@@ -0,0 +1,7 @@
+---
+title: Hello world!
+description: This is my first blog post.
+pubDate: 2023-06-16
+---
+
+I have created this file so that Astro will not error.
